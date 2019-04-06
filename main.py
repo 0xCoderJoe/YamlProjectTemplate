85f3ea5daccc7c5b7a4b516f21865c4ae353be01
0xCoderJoe <0xCoderJoe@protonmail.com>
2019-04-06 08:35:38 -1000

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+# Install Pyaml
+import yaml
+
+
+# A default main function
+def main():
+
+    # Read and parse our yaml configuration
+    with open('config.yaml', 'r') as yaml_file:
+        configObject = yaml.parse(yaml_file)
+        pprint(configObject)
+
+
+if __name__ == '__main__':
+    main()
